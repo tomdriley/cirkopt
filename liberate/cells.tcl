b50375f95de2771b3a7d3610ce2a3e1ec0814b0d
Tom Riley <mail@thomasriley.ca>
2021-01-31 10:24:14 -0500

    update liberate files to match cadence rak

--- liberate/cells.tcl
+++ liberate/cells.tcl
@@ -1,8 +1,10 @@
 # Comment
 set cells {
   AND2X1
+  BUFX8
+  BUFX8_drv
   INVX1
   XOR2X1
 }
 # Overrides
-set cells { AND2X1 INVX1 }
+set cells { INVX1 }
--- liberate/tcl/settings.tcl
+++ liberate/tcl/settings.tcl
@@ -2,6 +2,10 @@
 #####----------------------------------------
 ##### Set liberate variables
 #####----------------------------------------
+### Get tool version ###
+lassign [split [ALAPI_version] .] x1 x2 version(minor) version(sub)
+set version(major) "${x1}.${x2}"
+
 ### External Simulator (Spectre) settings ###
 set_var extsim_cmd_option      "+aps +spice -mt +liberate +rcopt=2"
 set_var extsim_deck_header     "simulator lang=spectre\nOpt1 options reltol=1e-4 \nsimulator lang=spice"
@@ -13,20 +17,20 @@ set_var extsim_leakage_option  "redefinedparams=ignore hier_ambiguity=lower limi
 set_var ski_enable               1
 set_var ski_clean_mode           1  ;# run $ALTOSHOME/bin/clean_sm.sh to clean up inactive semaphores
 set_var ski_compatibility_mode   1
-
+set_var power_tend_match_tran    1  ;# use tran_tend for power_tend for non-ski (match ski)
 
 ### Misc ###
 set_var parse_auto_define_leafcell   0 ;# disable auto leaf cell determination
 set_var tmpdir /dev/shm          ;# /dev/shm - use local RAM disk for tmp dir, /tmp - use local disk
 set_var extsim_deck_dir [file normalize "decks"]   ;# specify directory for SPICE decks and output files
+set_var set_var_failure_action   error
 
 
 ### Input waveform ###
-set_var predriver_waveform       2 ;# use pre-driver waveform
+set_var predriver_waveform       2 ;# 2=use pre-driver waveform
 
 
 ### Arc Generation
-set_var force_condition              4
 
 
 ### Capacitance ###
@@ -38,13 +42,21 @@ set_var min_capacitance_for_outputs            1        ;# write min_capacitance
 
 
 ### Timing ###
+set_var force_condition              4
+
+# tristate cell
+#set_var disable_method              2 ;# 2=measure disable timing from input 50% -> output short circuit current<10%
+#set_var tristate_disable_transition 0 ;# 0=output value=0 for three_state_disable transition
 
 
 ### Constraint ###
-#set_var constraint_info                  2
-#set_var constraint_search_time_abstol    1e-12	;# 1ps resolution for bisection search
+set_var constraint_info                  2
+#set_var constraint_search_time_abstol    1e-12    ;# 1ps resolution for bisection search
 set_var nochange_mode                    1        ;# enable nochange_* constraint characterization
-
+#avail version 17.1.2
+if {($version(major)>=17.1) && ($version(minor)>=2)} {
+    set_var constraint_vector_mode           4        ;# look at metric=delay and glitch to determine worst case
+}
 
 ### min_pulse_width ###
 set_var conditional_mpw            0       ;# 0=disable conditional mpw
@@ -58,13 +70,17 @@ set_var reset_negative_leakage_power       1            ;# convert negative leak
 
 ### Power ###
 set_var voltage_map                         1	;# create pg_pin groups, related_power_pin / related_ground_pin
-set_var pin_based_power                     0	;# Monitor power based on Vdd pin only
-set_var power_multi_output_binning_mode	    1
-set_var power_subtract_leakage              4	;# use 4 for cells with exhaustive leakage states.
-set_var subtract_hidden_power               2   ;# subtract hidden power for all cells
-set_var subtract_hidden_power_use_default   2   ;# subtract hidden power for overlapping when, then default group
+set_var pin_based_power                     0	;# 0=based on VDD only; 1=power based on VDD and VSS (default); 
+set_var power_combinational_include_output  0   ;# do not include output pins in when conditions for combinational cells
 
-### Hidden Power ###
+set_var force_default_group                 1
+set_default_group -criteria                 {power avg}  ;# use average for default power group
+
+set_var power_subtract_leakage              4
+set_var subtract_hidden_power               2   ;# 1=subtract hidden power for all cells
+set_var subtract_hidden_power_use_default   3   ;# 3=subtract hidden power from matched when condition then default group
+set_var power_multi_output_binning_mode     1   ;# binning for multi-output cell considered for both timing and power arcs
+set_var power_minimize_switching            1
 set_var max_hidden_vector                   [expr 2**10]
 
 
@@ -74,9 +90,16 @@ set_var max_hidden_vector                   [expr 2**10]
 ### CCSN ###
 set_var ccsn_include_passgate_attr 1  ;# include pin level attribute is_unbuffered and has_pass_gate, and
                                        # ccsn_*_stage group level attribute is_pass_gate
-
+set_var ccsn_model_related_node_attr 1 ;# enable output of ccsn_first_stage attribute
+                                        #   related_spice_node, load_cap_rise, load_cap_fall
+#set_var ccsn_tempus_promote_mode 1  ;# same as ccsn_model_related_node_attr  
 
 ### CCSP ###
+set_var ccsp_min_pts              15   ;# CCSP accuracy
+set_var ccsp_rel_tol              0.01 ;# CCSP accuracy
+set_var ccsp_table_reduction      0    ;# CCSP accuracy
+set_var ccsp_tail_tol             0.02 ;# CCSP accuracy
+set_var ccsp_related_pin_mode     2    ;# use 3 for multiple input switching scnarios and Voltus only libraries
 
 
 ## ECSM ##
@@ -90,7 +113,6 @@ set_var ccsn_include_passgate_attr 1  ;# include pin level attribute is_unbuffer
 
 ## EM ##
 if { [info exists CHAR_EM_TECH_FILE] && ($CHAR_EM_TECH_FILE ne "") } { set_var em_tech_file [file normalize $CHAR_EM_TECH_FILE] }
-set_var em_char_arcs_mode   1                 ;# test only high (RF) output pulse; default=test both high (RF) and low (FR) pulse
 
 
 #####----------------------------------------
@@ -98,10 +120,13 @@ set_var em_char_arcs_mode   1                 ;# test only high (RF) output puls
 #####----------------------------------------
 set_var write_library_is_unbuffered            1
 set_var cell_use_both_ff_latch_groups          2 ;# allow use of multiple ff,latch,state_table groups in userdata file
-set_var user_data_override { power_down_function pg_pin }
+set_var user_data_override { power_down_function pg_pin input_signal_level output_signal_level }
 set_var sdf_cond_style                         1
 set_var parenthesize_not                       0 ;# use !A instead of !(A)
 set_var driver_type_model_pad_check            1 ;# enable fix to disable output of driver_type pin attribute for tie cells CCR1407896
+set_var ccsn_print_is_needed_if_false_attr_value 1
+set_default_group -criteria {constraint off}    ;# disable writing of default constraint arc
+set_var write_library_allow_switching_and_hidden_power 1  ;# allow writing of switching and hidden power for same pin
 
 #####----------------------------------------
 ##### liberate_lv
@@ -120,9 +145,9 @@ if { $::LIBERATE_program == "VARIETY" } {
                                                 # use default of 0 for AOCV(fastest)
     #####  Constraint  #####
     #set_var constraint_random_variation_search_time_abstol [expr [get_var constraint_search_time_abstol] * 0.1] ;# typically set to 10% of constraint_search_time_abstol
-    set_var lvf_constraint_early_late_mode   1 ;# output early and late sigma type for constraint
+    set_var lvf_constraint_early_late_mode   1 ;# 1=output early and late sigma type for constraint
                                                 #   more accruate for non-gaussian distribution
-    set_var mpw_variation                    1 ;# enable mpw variation characterization
+    # set_var mpw_variation                    1 ;# enable mpw variation characterization
 
     #####  Monte Carlo  #####
     set_var extsim_monte_option  "sampling=lds" ;# set Spectre Monte Carlo sampling method [standard lhs lds orthogonal]
