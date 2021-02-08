0747c17d4ebdb8ee5f8dff8276cb24eb29b9ec05
Manju Weerasinghe <manju@qed.software>
2021-02-08 14:00:59 -0500

    Change to spaces, move cmd line arg get to separate function

--- python/src/util.py
+++ python/src/util.py
@@ -3,37 +3,42 @@ import os
 from os import path
 
 
-def cell_input():
-	cell_names = [""]
-	replaced = False
+def cell_input(file_path, file_name, cell_names):
+    replaced = False
 
-	file_path = input( "Enter path to file:")
-	file_name = input( "Enter file name:")
-	cell_names_str = input( "Enter cell names, separted by comma, no space:" )
+    old_path = f"{file_path}/bk_{file_name}"
+    new_path = f"{file_path}/{file_name}"
 
-	# Use if there can only be uppercase lettered cells
-	cell_names_str.upper()
+    if path.exists(new_path):
+        # rename the original file to create a backup
+        os.rename(new_path, old_path)
 
-	if cell_names_str is not None:
-		cell_names = cell_names_str.split( ',' )
+        with open( old_path, 'rt' ) as fin, open( new_path, 'wt' ) as fout:
+            for line in fin.readlines():
+                if not replaced:
+                    if line.startswith( 'set cells' ):
+                        replaced = True
+                        fout.write( line )
+                    else:
+                        fout.write( line )
+                elif line.startswith( '}' ):
+                    for cell in cell_names:
+                        fout.write(f"    {cell}\n")
+                    fout.write( line )
+                    replaced = False
 
-	old_path = f"{file_path}/bk_{file_name}"
-	new_path = f"{file_path}/{file_name}"
 
-	if path.exists(new_path):
-		# rename the original file to create a backup
-		os.rename(new_path, old_path)
+def get_file_params():
+    cell_names = ['']
 
-		with open( old_path, 'rt' ) as fin, open( new_path, 'wt' ) as fout:
-			for line in fin.readlines():
-				if not replaced:
-					if line.startswith( 'set cells' ):
-						replaced = True
-						fout.write( line )
-					else:
-						fout.write( line )
-				elif line.startswith( '}' ):
-					for cell in cell_names:
-						fout.write(f"    {cell}\n")
-					fout.write( line )
-					replaced = False
+    file_path = input( "Enter path to file:")
+    file_name = input( "Enter file name:")
+    cell_names_str = input( "Enter cell names, separted by comma, no space:" )
+
+    # Use if there can only be uppercase lettered cells
+    cell_names_str.upper()
+
+    if cell_names_str is not None:
+        cell_names = cell_names_str.split( ',' )
+
+    return file_path, file_name, cell_names
