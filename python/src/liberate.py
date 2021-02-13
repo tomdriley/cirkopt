1d4d955365c38ddb86bab823c0157070764b2fb6
Tom Riley <mail@thomasriley.ca>
2021-02-13 17:43:00 -0500

    Replicated run script, except no log capture

---
+++ python/src/liberate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import subprocess
+import os.path
+
+# Liberate project folder is defined relative to the location of this script
+PYTHON_SRC_DIRECTORY: str = os.path.dirname(os.path.abspath(__file__))
+LIBERATE_DEFAULT_PROJECT_DIRECTORY: str = os.path.abspath(
+    os.path.join(PYTHON_SRC_DIRECTORY, "../../liberate")
+)
+CHAR_TCL_DEFAULT_PATH: str = os.path.join(
+    LIBERATE_DEFAULT_PROJECT_DIRECTORY, "tcl/char.tcl"
+)
+# Liberate executable location assumes you are running on University servers by default
+LIBERATE_DEFAULT_PATH: str = "/CMC/tools/cadence/LIBERATE18.10.293_lnx86/bin/liberate"
+
+
+def run_liberate(
+    liberate_path: str = LIBERATE_DEFAULT_PATH,
+    char_tcl_path: str = CHAR_TCL_DEFAULT_PATH,
+    run_dir: str = LIBERATE_DEFAULT_PROJECT_DIRECTORY,
+) -> None:
+    """Run Cadence Liberate
+
+    characterizes SPICE (.sp) files and generate Liberty library (.lib or .ldb)
+    """
+
+    # TODO: Add error checking that files given exist
+
+    result = subprocess.run(args=[liberate_path, char_tcl_path], cwd=run_dir)
+
+
+if __name__ == "__main__":
+    run_liberate()
