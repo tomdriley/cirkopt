3c6913f23ae30d6edc85ff94df69cd8953271c33
jameswolfeoliver <jamesoliver88@gmail.com>
2021-02-18 23:22:00 -0500

    Graph some stuff from example netlist

--- python/scripts/single_param_sweep.py
+++ python/scripts/single_param_sweep.py
@@ -2,16 +2,18 @@ import os.path
 
 from src.file_io import File
 from src.liberty_parser import LibertyParser
+from src.liberate_grapher import graph_cell_delay
 from src.netlist import BaseNetlistFile, Netlist
 from src.single_param_sweep import NoopCostFunction, Param, \
     ParamSweepCandidateGenerator, SingleParamSweep
 
 
 def main():
-    # TODO: use command line args instead of hard coding
+    # # TODO: use command line args instead of hard coding
     curr_path = os.path.abspath(os.path.dirname(__file__))
     sim_result_path = os.path.join(curr_path, "../../liberate/{}/{}.ldb")
     sim_file = File(sim_result_path)
+
 
     reference_netlist_name = "INVX1"
     reference_netlist_path = os.path.join(
@@ -27,12 +29,14 @@ def main():
         netlist_file = File(path)
         netlist.persist(netlist_file)
 
+    widths = [i * 100e-9 for i in range(2, 11)]
+
     # TODO: add some mechanism to parametrize new W/L/M values via command line
     candidate_generator = ParamSweepCandidateGenerator(
         reference_netlist=reference_netlist,
         netlist_persister=persist_netlist_in_run_dir,
         param=Param.WIDTH,
-        values=(260e-9, 310e-9)
+        values=widths
     )
     cost_function = NoopCostFunction()
     liberty_parser = LibertyParser()
@@ -42,9 +46,17 @@ def main():
         liberty_parser,
         sim_file
     )
-    single_param_sweep.search()
+    # single_param_sweep.search()
 
-    # TODO: Graph the results of simulation
+    # Graph the results of simulation
+    graph_cell_delay(
+        sim_file=sim_file,
+        pin="Y",
+        delay_index=(0, 1),
+        x_axis=widths,
+        x_axis_title="Width",
+        out_path="out/sweep-width.png"
+    )
 
 
 if __name__ == '__main__':
