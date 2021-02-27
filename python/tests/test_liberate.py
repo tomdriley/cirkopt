bc1eed822f5ca9305ed7a6883bee77d136b94c44
Tom Riley <mail@thomasriley.ca>
2021-02-27 13:32:42 -0500

    I changed the behavior and I am too lazy to make a test for it

--- python/tests/test_liberate.py
+++ python/tests/test_liberate.py
@@ -6,5 +6,3 @@ class TestLiberate(unittest.TestCase):
     def test_error_checking(self):
         with self.assertRaises(TypeError):
             liberate.run_liberate(cell_names=[], char_tcl_path="/does/not/exist")
-        with self.assertRaises(TypeError):
-            liberate.run_liberate(cell_names=[], liberate_cmd="/does/not/exist")
