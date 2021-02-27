6df5e28e9c92db7031dc462bdfe63d17ac2bb116
Tom Riley <mail@thomasriley.ca>
2021-02-27 15:35:20 -0500

    Supress annoying prints from matplotlib

--- python/src/liberate_grapher.py
+++ python/src/liberate_grapher.py
@@ -1,4 +1,6 @@
 from typing import Sequence, Tuple
+import logging
+from logging import info, debug
 
 import matplotlib.pyplot as plt  # type: ignore
 
@@ -8,12 +10,12 @@ from src.utils import single
 
 
 def graph_cell_delay(
-        sim_file: IFile,
-        pin: str,
-        delay_index: Tuple[int, int],
-        x_axis: Sequence[float],
-        x_axis_title: str,
-        out_path: str
+    sim_file: IFile,
+    pin: str,
+    delay_index: Tuple[int, int],
+    x_axis: Sequence[float],
+    x_axis_title: str,
+    out_path: str,
 ) -> None:
     liberty_parser = LibertyParser()
     library = liberty_parser.parse(sim_file)
@@ -21,27 +23,40 @@ def graph_cell_delay(
     di1, di2 = delay_index
     # pylint: disable=no-member
     rise_times = [
-        single(lambda p: p.name == pin, cell.pin).timing[0].cell_rise[0].values[di1][di2]
+        single(lambda p: p.name == pin, cell.pin)
+        .timing[0]
+        .cell_rise[0]
+        .values[di1][di2]
         for cell in library.cell  # type: ignore
     ]
     fall_times = [
-        single(lambda p: p.name == pin, cell.pin).timing[0].cell_fall[0].values[di1][di2]
+        single(lambda p: p.name == pin, cell.pin)
+        .timing[0]
+        .cell_fall[0]
+        .values[di1][di2]
         for cell in library.cell  # type: ignore
     ]
 
     assert len({len(rise_times), len(fall_times), len(x_axis)}) == 1
 
+    info("Generating graph of results.")
+
+    mpl_logger = logging.getLogger("matplotlib")
+    if logging.getLogger().level < logging.WARNING:
+        debug("Suppressing matplotlib logs below warning.")
+        mpl_logger.setLevel(logging.WARNING)
+
     plt.figure()
     plt.title(f"{x_axis_title} vs Rise and Fall time")
-    plt.plot(x_axis, rise_times, 'b')
-    plt.plot(x_axis, fall_times, 'g')
-    plt.legend(['cell rise', 'cell fall'])
-    plt.ylabel('Delay')
+    plt.plot(x_axis, rise_times, "b")
+    plt.plot(x_axis, fall_times, "g")
+    plt.legend(["cell rise", "cell fall"])
+    plt.ylabel("Delay")
     plt.xlabel(x_axis_title)
     plt.tight_layout()
     ax = plt.gca()
     ax.set_axisbelow(True)
     ax.minorticks_on()
-    ax.grid(which='major', linestyle='-', linewidth='0.5', color='red')
-    ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+    ax.grid(which="major", linestyle="-", linewidth="0.5", color="red")
+    ax.grid(which="minor", linestyle=":", linewidth="0.5", color="black")
     plt.savefig(out_path)
