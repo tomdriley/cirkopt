81e863637ae7a4e4a2c09f6668fe784f34f991e9
Tom Riley <mail@thomasriley.ca>
2021-03-07 09:34:27 -0500

    Create larger set of netlist examples

--- python/tests/test_netlist.py
+++ python/tests/test_netlist.py
@@ -3,24 +3,10 @@ import textwrap
 
 from src.netlist import BaseNetlistFile, Netlist
 from tests.mock_file import MockFile
-
-
-TEST_NETLIST = """
-** Library name: gsclib045
-** Cell name: INVX1_3
-** View name: schematic
-.subckt INVX1_3 A Y VDD VSS
-*.PININFO  VSS:I VDD:I A:I Y:O
-** Above line required by Conformal LEC - DO NOT DELETE
-
-mp0 Y A VDD VDD g45p1svt L=45e-9 W=250e-9 AD=54.6e-15 AS=54.6e-15 PD=1.06e-6 PS=1.06e-6 NRD=358.974e-3 NRS=358.974e-3 M=1
-mn0 Y A VSS VSS g45n1svt L=50e-9 W=300e-9 AD=36.4e-15 AS=36.4e-15 PD=800e-9 PS=800e-9 NRD=538.462e-3 NRS=538.462e-3 M=2
-.ends INVX1_3
-"""
+from tests.netlist_example import TEST_NETLIST
 
 
 class TestNetlist(unittest.TestCase):
-
     def test_base_netlist_file(self):
         mock_file = MockFile()
         mock_file.write("some content")
@@ -51,7 +37,7 @@ class TestNetlist(unittest.TestCase):
             cell_name="INVX1_4",
             device_widths=(260e-9, 310e-9),
             device_lengths=(50e-9, 60e-9),
-            device_fingers=(2, 3)
+            device_fingers=(2, 3),
         )
 
         new_mock_file = MockFile()
@@ -70,7 +56,4 @@ class TestNetlist(unittest.TestCase):
             .ends INVX1_4
             """
 
-        self.assertEqual(
-            new_mock_file.read(),
-            textwrap.dedent(expected_netlist_string)
-        )
+        self.assertEqual(new_mock_file.read(), textwrap.dedent(expected_netlist_string))
