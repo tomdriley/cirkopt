3ba52ee129dc615479b02097b2acc308ba67d9ab
jameswolfeoliver <jamesoliver88@gmail.com>
2021-03-07 23:15:20 -0500

    Do it all

---
+++ python/src/genetic_search.py
@@ -0,0 +1,239 @@
+from itertools import chain
+from math import ceil, log10
+from typing import Callable, List, Optional, Sequence, Tuple
+
+from numpy.random import default_rng
+from numpy.typing import ArrayLike
+import numpy as np
+
+from src.fixed_point import fixed, floating, Rounding
+from src.liberate import liberate_simulator
+from src.liberty_parser import LibertyResult
+from src.netlist import Netlist
+from src.search_algorithm import (
+    CandidateGenerator,
+    CostMap,
+    SearchAlgorithm,
+)
+
+CostFunction = Callable[[Sequence[Netlist], LibertyResult], CostMap]
+
+
+def _normalize(netlist: Netlist, precision: float) -> Tuple[int]:
+    """Returns the netlist's fixed point widths, fixed point lengths and fingers in a 1d tuple"""
+    widths = (fixed(w, precision) for w in netlist.device_widths)
+    lengths = (fixed(l, precision) for l in netlist.device_lengths)
+    fingers = iter(netlist.device_fingers)
+    return tuple(chain(widths, lengths, fingers))
+
+
+def _denormalize(normalized_netlist: ArrayLike, precision: float) -> Tuple[Tuple[float], Tuple[float], Tuple[int]]:
+    """Returns the normalized netlist's floating point widths, floating point lengths and fingers"""
+    ndevices = normalized_netlist.shape[0]
+    return (
+        tuple(floating(w, precision) for w in normalized_netlist[:ndevices]),
+        tuple(floating(l, precision) for l in normalized_netlist[ndevices:-ndevices]),
+        tuple(normalized_netlist[-ndevices:])
+    )
+
+
+class GeneticCandidateGenerator(CandidateGenerator[Netlist]):
+    # Population params
+    _num_individuals: int  # Number of candidates in a population
+    _elitism: bool  # Whether the best candidate should be included in the next generation
+    _n_points: int  # How many points to perform arithmetic crossover on two parents
+    _alpha: float  # Alpha for arithmetic crossover
+    _pmutation: float  # Probability a mutation will occur to a device parameter
+    _mutation_std_deviation: float  # Standard deviation of additive gaussian noise mutation
+
+    # Search space params
+    _min_width: int  # fixed point
+    _max_width: int  # fixed point
+    _min_length: int  # fixed point
+    _max_length: int  # fixed point
+    _precision: float
+    _fingers_values: Sequence[int]
+
+    _reference_netlist: Netlist
+    _number_of_devices: int
+    _netlist_persister: Optional[Callable[[Netlist], None]]
+
+    def __init__(
+            self,
+            num_individuals: int,
+            elitism: int,
+            min_width: int,
+            max_width: int,
+            min_length: int,
+            max_length: int,
+            precision: float,
+            fingers_values: Sequence[int],
+            reference_netlist: Netlist,
+            netlist_persister: Optional[Callable[[Netlist], None]]
+    ):
+        self._num_individuals = num_individuals
+        self._elitism = elitism
+
+        self._min_width = fixed(min_width, precision, Rounding.UP)
+        self._max_width = fixed(max_width, precision, Rounding.DOWN)
+        self._min_length = fixed(min_length, precision, Rounding.UP)
+        self._max_length = fixed(max_length, precision, Rounding.DOWN)
+        self._precision = precision
+        self._fingers_values = fingers_values
+
+        self._reference_netlist = reference_netlist
+        self._number_of_devices = len(reference_netlist.device_widths)
+        self._id_num_digits = ceil(log10(self._num_individuals))
+        self._netlist_persister = netlist_persister
+
+        self.rng = default_rng()
+
+    def get_initial_population(self) -> Sequence[Netlist]:
+        """
+        Generate N (self._num_individuals) netlists where each netlist has random device widths, lengths and fingers.
+        The randomized widths, lengths and fingers follow a normal distribution and satisfy the constraints given in
+        class initializer.
+
+        :return: a sequence of randomly generated netlists which have been persisted to disk.
+        """
+        def rand_netlist(id: int) -> Netlist:
+            # Generate fixed point widths and lengths
+            widths = self.rng.integers(self._min_width, self._max_width, self._number_of_devices, endpoint=True)
+            lengths = self.rng.integers(self._min_length, self._max_length, self._number_of_devices, endpoint=True)
+            # randomly choose finger values from provided values
+            fingers = self.rng.choice(self._fingers_values, self._number_of_devices)
+
+            return self._reference_netlist.mutate(
+                cell_name=self._get_netlist_name(id),
+                device_widths=tuple(floating(w, self._precision) for w in widths),  # convert from fixed point
+                device_lengths=tuple(floating(l, self._precision) for l in lengths),
+                device_fingers=fingers
+            )
+
+        netlists = [rand_netlist(id) for id in range(self._num_individuals)]
+        self._persist_netlists(netlists)
+        return netlists
+
+    def get_next_population(self, current_candidates: Sequence[Netlist], cost_map: CostMap) -> Sequence[Netlist]:
+        # Ensure same order as current_candidates
+        costs = np.asarray([cost_map[n.key()] for n in current_candidates], dtype=np.float32)
+
+        # TODO: Make mating pool size configurable maybe?
+        # Candidates with lower cost have higher probability of being a parent
+        parenting_probabilities = 1 - (costs / costs.sum())
+
+        # Two NxM arrays where N is number of individuals and M is 3 * number of devices per individual
+        mating_pool = np.asarray([_normalize(n, self._precision) for n in current_candidates], dtype=np.uint16)
+        offspring = np.zeros((self._num_individuals, self._number_of_devices * 3), dtype=np.uint16)
+
+        def maybe_add_child_to_offspring(child_to_add: ArrayLike, child_idx: int) -> bool:
+            """Adds child if it doesn't already exist by some fluke. Also checks bounds. Returns if child was added"""
+            # Don't add if it would be out of bounds
+            if child_idx >= offspring.shape[0]:
+                return False
+
+            for idx, child in enumerate(offspring):
+                if idx > child_idx:
+                    break  # break early since from here on out it'll all be zeros
+
+                if child_to_add == child:
+                    return False  # if child already exists in offspring, don't add
+            # Child not found in offspring, so it can be added
+            offspring[child_idx] = child_to_add
+            return True
+
+        assert mating_pool.shape == offspring.shape
+
+        num_children = 0
+        while num_children < self._num_individuals:
+            # draw 2 parents from mating pool
+            parent_indices = np.random.choice(
+                a=mating_pool.shape[0],
+                size=2,
+                replace=False,
+                p=parenting_probabilities
+            )
+
+            # Perform crossover
+            parent_a, parent_b = mating_pool[parent_indices]
+            child_a, child_b = self.n_point_arithmetic_crossover(parent_a, parent_b)
+
+            # Perform mutation
+            child_a = self.guassian_mutation(child_a)
+            child_b = self.guassian_mutation(child_b)
+
+            if maybe_add_child_to_offspring(child_a, num_children):
+                num_children += 1
+            if maybe_add_child_to_offspring(child_b, num_children):
+                num_children += 1
+
+        # elitism
+        if self._elitism:
+            lowest_cost_parent = costs.argmin()
+            random_offspring = self.rng.choice(range(self._num_individuals), 1)
+            offspring[random_offspring] = mating_pool[lowest_cost_parent]
+
+        netlists: List[Optional[Netlist]] = [None] * self._num_individuals
+        for id, normalized_netlist in enumerate(offspring):
+            assert normalized_netlist.any(), f"Offspring[{id}] contained only zeros"
+
+            widths, lengths, fingers = _denormalize(normalized_netlist, self._precision)
+            netlists[id] = self._reference_netlist.mutate(self._get_netlist_name(id), widths, lengths, fingers)
+
+        self._persist_netlists(netlists)
+        return netlists
+
+    def n_point_arithmetic_crossover(
+            self,
+            parent_a: ArrayLike,
+            parent_b: ArrayLike,
+    ) -> Tuple[ArrayLike, ArrayLike]:
+        assert len(parent_a) == len(parent_b)
+
+        # Select n random indices to crossover
+        points = self.rng.choice(range(len(parent_a)), self._n_points)
+
+        mix_mask = np.zeros_like(parent_a, dtype=np.bool8)
+        mix_mask[points] |= True
+        keep_mask = np.invert(mix_mask)
+
+        a = self._alpha
+        # Perform crossover
+        # for k in points: child_a[k] = alpha * parent_a[k] + (1-alpha) * parent_b[k]
+        child_a = (parent_a * keep_mask) + (a * mix_mask * parent_a) + ((1-a) * mix_mask * parent_b)
+        child_b = (parent_b * keep_mask) + ((1-a) * mix_mask * parent_a) + (a * mix_mask * parent_b)
+
+        return child_a.round().astype(np.uint16), child_b.round().astype(np.uint16)
+
+    def guassian_mutation(self, individual: ArrayLike) -> ArrayLike:
+        """Apply additive gaussian noise to each device parameter with a probability of self._pmutation"""
+        p = self._pmutation
+        mutation_mask = self.rng.choice([0, 1], len(individual), p=[1-p, p]).asType(np.bool8)
+        mutations = self.rng.normal(0, 1, len(individual))
+        return individual + mutations * mutation_mask
+
+    def _get_netlist_name(self, id: int) -> str:
+        return self._reference_netlist.cell_name + "_" + str(id).zfill(self._id_num_digits)
+
+    def _persist_netlists(self, netlists: Sequence[Netlist]):
+        assert self._netlist_persister is not None
+        for netlist in netlists:
+            self._netlist_persister(netlist)
+
+
+class GeneticSearch(SearchAlgorithm):
+    _max_iterations: int
+
+    def __init__(
+        self,
+        candidate_generator: GeneticCandidateGenerator,
+        cost_function: CostFunction,
+        max_iterations: int
+    ):
+        self._candidate_generator = candidate_generator
+        self._cost_function = cost_function
+        self._simulate = liberate_simulator
+        self._max_iterations = max_iterations
+
+    def _should_stop(self) -> bool:
+        return self._iteration >= self._max_iterations
